f18357891e1a269dcabb77e1a31130f3a65442a1
Kedar Sanjaykumar Swami <98695336+iamKed@users.noreply.github.com>
2023-04-12 19:45:26 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -3,28 +3,29 @@ from flask_sqlalchemy import SQLAlchemy
 import numpy as np
 import pickle
 import os
-import cv2
 from keras.utils import load_img,img_to_array
 from keras.models import load_model
 from keras.preprocessing import image
 import warnings
-from sklearn.preprocessing import StandardScaler as std_scaler
+from sklearn.preprocessing import StandardScaler
+from sqlalchemy import Engine, create_engine, select 
 warnings.filterwarnings('ignore')
-model= load_model(r"Trained_Models\modelHD.h5")
-model_diabetes=load_model(r'Trained_Models\modelDibt.h5')
+model= pickle.load(open("Trained_Models/heart.pkl",'rb'))
+model_diabetes=pickle.load(open("Trained_Models/diebites_model_best.pkl",'rb'))
 from gevent.pywsgi import WSGIServer 
+# engine=create_engine('sqlite:///database.db')
 from werkzeug.utils import secure_filename
 mri_model=load_model(r'Trained_Models\BrainTumor.h5')
 app=Flask(__name__)
 app.secret_key = b'82736781_@*@&(796*5&^5)'
 covid_19_model=load_model(r"Trained_Models\covid_model.h5")
-# bone_model=load_model(r"Trained_Models\bone_fracture.h5")
+bone_model=load_model(r"Trained_Models\best_model_bone.h5")
 skin_model=load_model(r"Trained_Models\skin_d.h5")
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-LOGIN_STATUS=False
-USERNAME=str('')
+app.config['LOGIN_STATUS']=False
+app.config['USERNAME']=str('')
 answer=''
 app.config['UPLOAD_FOLDER']=r'static/files'
 class Mainn(db.Model):
@@ -50,37 +51,6 @@ class New_Authentication(db.Model):
     age=db.Column(db.Integer, nullable=False)
 #preprocessing codes
 
-
-
-#preprocessing and prediction code for heart_disease_prediction module
-# def predict_heart_condition(age1,sex1,cp1,trestbps1,chol1,fbs1,restecg1,thalach1,exang1,oldpeak1,slope1,ca1,thal1):
-#     age=age1
-#     sex=sex1
-#     cp_list=['Typical Angina','Atypical Angina','Non-Anginal pain','Asymptomatic']
-#     restecg_list=['Normal','Having ST-T wave abnormality','Showing probable or definite left ventricular hypertrophy']
-#     cp=cp_list.index(cp1)
-#     restecg=restecg_list.index(restecg1)
-#     thalach=thalach1
-#     fbs_list=['Fasting blood sugar < 120','Fasting blood sugar > 120']
-#     fbs=fbs_list.index(fbs1)
-#     trestbps=trestbps1
-#     chol=chol1
-#     exang_list=['No','Yes']
-#     exang=exang_list.index(exang1)
-#     input_data=[int(age),int(sex),int(cp),int(trestbps),int(chol),int(fbs),int(restecg),int(thalach),int(exang),int(oldpeak1),slope1,int(ca1),int(thal1)]
-#     print(i for i in input_data)
-#     input_data=tuple(input_data)
-#     input_data=np.asarray(input_data)
-#     input_data=input_data.reshape(1,-1)
-#     print(input_data)
-#     Prediction=model.predict(input_data)
-#     if Prediction==[0]:
-#         return "Don't Worry You Don't Have a serious Heart Condition"
-#     else:
-#         return ""
-
-
-
 #preprocessing for brain Tumor module
 def preprocess_img_mri(img_path):
     xtest_image = load_img(img_path, target_size=(150,150,3))
@@ -92,25 +62,55 @@ def preprocess_img_mri(img_path):
 @app.route('/login',methods=['GET','POST'])
 def login_user():
     if request.method=='POST':
-        email=request.form['mail']
+        mail=request.form['mail']
         password=request.form['password']
-        print(email,password)
-    # if condition check:
-    else:
-        return render_template('login.html')
+        print(mail,password)
+        user = New_Authentication.query.filter_by(email=mail).first()
+        
+        # print((user,passw))
+        if user:
+            # passw=New_Authentication.query.filter_by(password=password)
+            # print(select(New_Authentication.password).where(New_Authentication.email == mail))
+            with db.engine.connect() as conn:
+                result = conn.execute(select(New_Authentication.password).where(New_Authentication.email==mail))
+                passw=(str(result.all()[0][0]))
+                name = conn.execute(select(New_Authentication.name).where(New_Authentication.email==mail))
+                app.config['USERNAME']=(str(name.all()[0][0]))
+                print(app.config['USERNAME'],passw)
+
+
+            # c = conn.cursor()
+            # c.execute(f"SELECT * FROM New_Authentication WHERE email='{email}'")
+            # rows = c.fetchall()
+            # for row in rows:
+            #     print(row)
+            if passw==password:
+            # print(New_Authentication.query.filter_by(email=email,password=password).first())
+                app.config['LOGIN_STATUS']=True
+                # app.config['app.config['USERNAME']']=user
+                # app.config['app.config['USERNAME']']=New_Authentication.name.filter_by(email=email,password=password)
+                print(app.config['USERNAME'])
+                return redirect('/')
+            else:
+                app.config['USERNAME']=''
+                tag="Wrong password"
+                return render_template('login.html',alrmsg=tag)
+        else:
+            print("No User")
+    return render_template('login.html')
 
 
 @app.route('/logout',methods=['GET','POST'])
 def logout_user():
-    LOGIN_STATUS=False
-    USERNAME=False
-    return render_template('homepage.html',status=LOGIN_STATUS,username=USERNAME)
+    app.config['LOGIN_STATUS']=False
+    app.config['USERNAME']=''
+    return redirect('/')
 
 
 @app.route('/')
 def refresh():
-    print(app.root_path)
-    return render_template("homepage.html",status=LOGIN_STATUS,username=USERNAME)
+    print("Ked",app.config['USERNAME'],app.config['USERNAME'])
+    return render_template("homepage.html",status=app.config['LOGIN_STATUS'],username=app.config['USERNAME'])
 
 @app.route('/registration',methods=['GET', 'POST'])
 def registration():
@@ -121,36 +121,51 @@ def registration():
         gender=request.form['inlineRadioOptions']
         age=int(request.form['age'])
         password=request.form['password']
-        name=fn+ln
+        name=fn+' '+ln
         print(name,email,password,email,gender)
         new_auth=New_Authentication(name=name,email=email,password=password,gender=gender,age=age)
         db.session.add(new_auth)
         db.session.commit()
         print("Committed")
-        return render_template('login.html')
+        return redirect('/login')
     else:
         return render_template('registration.html')
     
 
 @app.route('/Covid_19')
 def Covid_19():
-    return render_template("Covid_19.html",status=LOGIN_STATUS,username=USERNAME)
+    return render_template("Covid_19.html",status=app.config['LOGIN_STATUS'],username=app.config['USERNAME'])
 
 @app.route('/Heart_Disease_Prediction')
 def Heart_Disease_Prediction():
-    return render_template("Heart_Disease.html",status=LOGIN_STATUS,username=USERNAME)
+    return render_template("Heart_Disease.html",status=app.config['LOGIN_STATUS'],username=app.config['USERNAME'])
 
+@app.route('/diabetes',methods=['GET','POST'])
+def diabetes():
+    if request.method=='POST':
+        # diver code for testing the model
+        para=['HighBP', 'HighChol', 'CholCheck', 'BMI', 'Smoker', 'Stroke',
+               'HeartDiseaseorAttack', 'PhysActivity', 'Veggies', 'HvyAlcoholConsump',
+               'GenHlth', 'MentHlth', 'PhysHlth', 'DiffWalk', 'Age', 'Education',
+               'Income'],
+        tip=[para]
+        res=model_diabetes.predict(np.array(tip))
+        if res[0]==0:
+            answer="Non-Diabetic"
+        else:
+            answer="Diabetic"
+    return render_template('diabetes.html',res=answer)
 @app.route('/Bone_Fracture_Detection')
 def Bone_Fracture_Detection():
-    return render_template("Bone_Fracture.html",status=LOGIN_STATUS,username=USERNAME)
+    return render_template("Bone_Fracture.html",status=app.config['LOGIN_STATUS'],username=app.config['USERNAME'])
 
 @app.route('/Skin_Cancer')
 def Skin_Cancer():
-    return render_template('Skin_Cancer.html',status=LOGIN_STATUS,username=USERNAME)
+    return render_template('Skin_Cancer.html',status=app.config['LOGIN_STATUS'],username=app.config['USERNAME'])
 
 @app.route('/Brain_Tumor_Detection')
 def Brain_Tumor_Detection():
-    return render_template("Brain_Tumor.html",status=LOGIN_STATUS,username=USERNAME)
+    return render_template("Brain_Tumor.html",status=app.config['LOGIN_STATUS'],username=app.config['USERNAME'])
 
 @app.route('/getmri',methods=['POST'])
 #prediction code for brain tumor detection
@@ -162,7 +177,7 @@ def getmri():
         file_path = os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(img.filename))
         print(file_path)
         result=preprocess_img_mri(file_path)
-        return render_template('Brain_Tumor.html',res=labels [(np.where(result[0]==1))[0][0] ])
+        return render_template('answer.html',res=labels[(np.where(result[0]==1))[0][0] ])
 
 @app.route('/getcovidresult',methods=['POST'])
 #prediction code for covid-19 module
@@ -181,7 +196,6 @@ def getcovidresult():
             predictions="Covid 19 Negative"
         elif predictions==1:
             predictions="Covid 19 Positive"
-        session['predictions'] = predictions
         # return render_template('Covid_19/html',res=predictions)
         return render_template('answer.html',res=predictions)
 
@@ -201,62 +215,57 @@ def getskin():
         disease_list = ["Eczema 1677","Melanoma","Atopic Dermantitis","Basal Cell Carcinoma (BCC)","Mealanocytic Nevi (NV)","Benign Keratosis-like Lessions (BKL)", "Psoriasis pictures lichen Planus and related diseases","Seaborrheic Keratoses and other Benign Tumors","Tinea Ringworm Candidiasis and other fungal infections","Warts Molluscum and other Viral Infections"]
         disease=disease_list[int(a[0]+1)]
         print(disease)
-    return render_template('Skin_Cancer.html',res=disease)
+    return render_template ('answer.html',res=disease)
 
 @app.route('/getbone',methods=['POST'])
 def getbone():
     if request.method=='POST':
-        labels=['Bone Not Fractured','Bone Fractured']
         img=request.files['bone']
         file_path=(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(img.filename)))
         img.save(file_path)
-        xtest_image = load_img(file_path, target_size=(224,224,3))
+        xtest_image = load_img(file_path, target_size=(224,224))
         xtest_image = img_to_array(xtest_image)
         xtest_image = np.expand_dims(xtest_image, axis = 0)
-        print(labels [(np.where(predictions[0]==1))[0][0] ])
-        print(predictions)
-        if predictions[0][0]==1:
-            predictions='Bone Not Fractured'
-        elif predictions[0][1]==1:
+        predictions=bone_model.predict(xtest_image)
+        predictions=np.argmax(predictions,axis=1)
+        print(predictions[0])
+        if predictions[0]==0:
             predictions='Bone Fractured'
+        else:
+            predictions='Bone Not Fractured'
         print(predictions)
-    return render_template('Bone_Fracture.html',res=predictions)
+    return render_template ('answer.html',res=predictions)
 
 
 @app.route('/getValue',methods=['POST'])
 def getValue():
     if request.method=='POST':
         oldpeak=float(request.form['oldpeak'])
-        slope=float(request.form['slope'])
-        ca=float(request.form['ca'])
-        thal=float(request.form['thal'])
-        sex=float(request.form['sex'])
-        age=float(request.form['age'])
-        cp=float(request.form['chaistpain'])
-        restecg=float(request.form['wave'])
-        thalach=float(request.form['hrate'])
-        fbs=float(request.form['bloodsugar'])
-        trestbps=float(request.form['bloodpressure'])
-        chol=float(request.form['serum'])
-        exang=float(request.form['anigna'])
+        slope=int(request.form['slope'])
+        ca=int(request.form['ca'])
+        thal=int(request.form['thal'])
+        sex=int(request.form['sex'])
+        age=int(request.form['age'])
+        cp=int(request.form['chaistpain'])
+        restecg=int(request.form['wave'])
+        thalach=int(request.form['hrate'])
+        fbs=int(request.form['bloodsugar'])
+        trestbps=int(request.form['bloodpressure'])
+        chol=int(request.form['serum'])
+        exang=int(request.form['anigna'])
         input_data=[age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal]
         print(input_data)
         answer=input_data
-        input_data=tuple(input_data)
-        input_data=np.asarray(input_data[:])
+        input_data=np.array(input_data)
+        input_data=input_data.reshape(1,-1)
         print("Input_Data: ",input_data)
-        # input_data=
-        input_data=std_scaler.transform(X=(input_data.reshape(1,-1)))
-        std_scaler.transform()
-        pred=model.predict(input_data)
-        print(pred)
-        answer=pred[0][0]>0.75
+        answer=model.predict(input_data)
         print(answer)
         if answer:
-            print("Don't Worry You Don't Have a serious Heart Condition")
+            answer="Don't Worry You Don't Have a serious Heart Condition"
         else:
-            print("Sorry to say that you might have a serious heart Condition. Please refer to a Doctor and Get treated!")
-        return render_template ('answer.html',ans=answer)
+            answer="Sorry to say that you might have a serious heart Condition. Please refer to a Doctor and Get treated!"
+        return render_template ('answer.html',res=answer)
 
 if __name__=="__main__":
     app.run(debug=True)
--- templates/main.js
+++ templates/main.js
@@ -1,7 +1,240 @@
-const previewImage= (event)=>{
-    const imgfiles=event.target.files;
-    const imageSrc=URL.createObjectURL(imgfiles[0]);
-    const imagePreviewElement=document.querySelector("#preview-selected-image");
-    
-    imagePreviewElement.src=imageSrc;
+const previewImage = (event) => {
+    const imgfiles = event.target.files;
+    const imageSrc = URL.createObjectURL(imgfiles[0]);
+    const imagePreviewElement = document.querySelector("#preview-selected-image");
+
+    imagePreviewElement.src = imageSrc;
+}
+
+let signup = document.querySelector(".signup");
+let login = document.querySelector(".login");
+let slider = document.querySelector(".slider");
+let formSection = document.querySelector(".form-section");
+
+signup.addEventListener("click", () => {
+    slider.classList.add("moveslider");
+    formSection.classList.add("form-section-move");
+});
+
+login.addEventListener("click", () => {
+    slider.classList.remove("moveslider");
+    formSection.classList.remove("form-section-move");
+});
+
+
+// change the border while entering the values
+
+// let iptag=document.querySelector(".ele");
+
+function A() {
+    slider.classList.remove("moveslider");
+    formSection.classList.remove("form-section-move");
+}
+function B() {
+    slider.classList.add("moveslider");
+    formSection.classList.add("form-section-move");
+}
+function CBorder(tag) {
+
+}
+
+
+// password eye icon to show and hide icon
+const togglePassword = document.querySelector('#togglePassword');
+const password = document.querySelector('#id_password');
+
+togglePassword.addEventListener('click', function (e) {
+    // toggle the type attribute
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    // toggle the eye slash icon
+    togglePassword.classList.toggle('fa-eye-slash');
+});
+
+const togglePassword1 = document.querySelector('#togglePassword1');
+const password1 = document.querySelector('#id_password1');
+
+togglePassword1.addEventListener('click', function (e) {
+    // toggle the type attribute
+    // window.alert("Toggle happend");
+    const type1 = password1.getAttribute('type') === 'password' ? 'text' : 'password';
+    password1.setAttribute('type', type1);
+    // toggle the eye slash icon
+    togglePassword1.classList.toggle('fa-eye-slash');
+});
+
+const togglePassword2 = document.querySelector('#togglePassword2');
+const password2 = document.querySelector('#id_password2');
+
+togglePassword2.addEventListener('click', function (e) {
+    // toggle the type attribute
+    const type2 = password2.getAttribute('type') === 'password' ? 'text' : 'password';
+    password2.setAttribute('type', type2);
+    // window.alert("function called.....");
+    // toggle the eye slash icon
+    togglePassword2.classList.toggle('fa-eye-slash');
+});
+
+
+// function Show(tag){
+//     let t=document.querySelector(tag);
+
+//     const type = t.getAttribute('type') === 'password' ? 'text' : 'password';
+//     t.setAttribute('type', type);
+//     // toggle the eye slash icon
+//     this.classList.toggle('fa-eye-slash');
+// }
+
+
+// +==================== Validation section ==================== 
+
+//  +++++++++ Password Strength checker using jQuery ++++++++++++++
+
+// $(function(){
+
+//   $('input#demo').passwordstrength({
+//   'minlength': 6, // Minimum length of password
+//   'number'   : true, // Password requires at least one number
+//   'capital'  : true, // Password requires at least one uppercase letter
+//   'special'  : true, // Password requires at least one special character
+//   'labels'   : {
+//   'general'   : 'The password must have :',
+//   'minlength' : 'At leaset {{minlength}} characters',
+//   'number': 'At least one number',
+//   'capital'   : 'At least one uppercase letter',
+//   'special'   : 'At least one special character'
+//   }
+//   });
+
+//   });
+
+
+//  =============================================
+
+
+let passwordInfo = document.getElementById('passwordInfo');
+let passwordInput = document.getElementById("id_password1");
+
+
+
+// define regex for different password stength
+let poorReg = /[a-z]/; // password consist of only letters
+let weakReg = /(?=.*?[0-9])/;
+let strongReg = /(?=.*?[#?!@$%^&*-])/;
+let whiteSpaceReg = /^$|\s+/;
+
+
+
+passwordInput.onkeyup = function () {
+    let passVal = passwordInput.value;
+    let passLen = passVal.length;
+
+    let poorPassword = passVal.match(poorReg);
+    let weakPassword = passVal.match(weakReg);
+    let strongPassword = passVal.match(strongReg);
+    let whiteSpace = passVal.match(whiteSpaceReg);
+
+
+    if (passVal != "") {
+        if (whiteSpace) {
+            passwordInfo.textContent = "White spaces are not allowed";
+        }
+        else {
+            poorPasswordStrength(passLen, poorPassword, weakPassword, strongPassword);
+            weakPasswordStrength(passLen, poorPassword, weakPassword, strongPassword);
+            strongPasswordStrength(passLen, poorPassword, weakPassword, strongPassword);
+            // passwordInfo.textContent="Use character, digits and symbol";
+            // passwordInfo.textContent="";
+        }
+    } else {
+        // passwordInfo.style.display='none';
+        passwordInfo.textContent = "Please enter your password";
+    }
+
+}
+
+function poorPasswordStrength(passLen, poorPassword, weakPassword, strongPassword) {
+
+    if (passLen <= 3 && (poorPassword || weakPassword || strongPassword)) {
+        // passwordInfo.style.display="block";
+        passwordInfo.style.color = 'red';
+        passwordInfo.textContent = "your password is too poor";
+        console.log("Poor Password function get called");
+    }
+}
+
+function weakPasswordStrength(passLen, poorPassword, weakPassword, strongPassword) {
+    //   console.log("Weak Password function get called");
+    if (passLen >= 6 && poorPassword && (weakPassword || strongPassword)) {
+        // passwordInfo.style.display="block";
+        passwordInfo.style.color = 'orange';
+        passwordInfo.textContent = "your password is Weak: Use Special symbol and digits";
+
+    } else {
+        passwordInfo.textContent = "Use Special symbol and length will be atleast 8";
+    }
+}
+
+function strongPasswordStrength(passLen, poorPassword, weakPassword, strongPassword) {
+    //   console.log("Strong Password function get called");
+    if (passLen >= 8 && (poorPassword && weakPassword) && strongPassword) {
+
+        // passwordInfo.style.display="block";
+        passwordInfo.style.color = 'green';
+        passwordInfo.textContent = "your password is Strong";
+
+    } else {
+        // passwordInfo.textContent="Use";
+        // passwordInfo.style.display='none';
+
+    }
+}
+
+// +++++++++++++++++++ Conform password validation ++++++++++++++++++++++++
+
+// var pw=document.getElementById("pid");
+var msg = document.getElementById('msg');
+function validation(tag) {
+    var cfpw = document.getElementById(tag);
+    var pw = document.getElementById("id_password1");
+    // console.log(cfpw.value);
+    // console.log(pw.value);
+    if (cfpw.value != pw.value) {
+        cfpw.style.border = "2px solid rgb(252, 148, 148)";
+        // msg.style.display='relative';
+        msg.textContent = "Please enter same password";
+        // return false;
+    }
+    else {
+        cfpw.style.border = "2px solid rgb(128, 247, 255)";
+        console.log("Password is matched....")
+        msg.style.display = 'none';
+        msg.textContent = '';
+        // return true;
+    }
+
+}
+function checkIt() {
+    var cfpw = document.getElementById("id_password2");
+    var pw = document.getElementById("id_password1");
+    if (cfpw.value != pw.value) {
+        console.log("Please enter valid password....")
+        window.alert("Please enter same password...")
+    }
+}
+
+
+// change the border color
+function changeB(tag) {
+    let t = document.querySelector(tag);
+    console.log(t.value);
+    console.log("inside the changeB");
+
+    t.style.border = '2px solid rgb(128, 247, 255)';
+    t.style.boxShadow = '2px black';
+
+}
+
+function myalert() {
+    alert("Wrong Password");
 }
